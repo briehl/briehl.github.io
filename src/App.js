974bf6a628677a7817a18dafa036c09bd3ade1f9
Bill Riehl <wjriehl@lbl.gov>
2019-03-01 20:09:39 -0800

    move css files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+          <NavBar />
+        </div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My Webpage of DOOM.</h1>
+          <h1 className="App-title">Welcome to My Webpage of sorta doom.</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    light: orange[200], // same as '#FFCC80',
+    main: '#FB8C00', // same as orange[600]
+    dark: '#EF6C00',
+    contrastText: 'rgb(0,0,0)',
+    primary: orange
+  }
+});
+
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
